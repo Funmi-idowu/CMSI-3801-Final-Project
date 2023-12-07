aa7d9e6f9b8551205f447b5bb2a88af948ff43e1
funmi idowu <fidowu@lion.lmu.edu>
2023-12-07 11:33:36 -0800

    adding fontawesome

--- deco/src/App.js
+++ deco/src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <FontAwesomeIcon icon={faPenNib} />
+      <FontAwesomeIcon icon={faEnvelope} />
+      <FontAwesomeIcon icon={faTrash} />
     </div>
   );
 }
